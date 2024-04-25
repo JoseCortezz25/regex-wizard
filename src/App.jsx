e034171aa3143bd279e5390a6b92cfc8afbf1a08
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-04-24 21:26:17 -0500

    Add react-ga4 package and initialize Google Analytics in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { Toaster } from "sonner";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import Presentation from "./components/Presentation/Presentation";
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
+
   return (
     <>
       <main className="Main">
